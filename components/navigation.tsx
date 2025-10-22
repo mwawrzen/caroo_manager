69b79da3df830004f0ae68fedf9b283cd275f238
mwawrzen <w.w@op.pl>
2025-10-23 01:55:42 +0200

    feat: update icons style

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,17 +1,18 @@
-import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
-import { FontAwesome } from "@expo/vector-icons";
+import { ThemedText } from "@/components/themed/themed-text";
+import { ThemedView } from "@/components/themed/themed-view";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import { Href, Link } from "expo-router";
 import { Pressable, StyleSheet } from "react-native";
+import { ThemedIcon } from "./themed/themed-icon";
 
 type NavigationOptionType = {
   href: Href,
-  icon: keyof typeof FontAwesome.glyphMap,
+  icon: keyof typeof FontAwesome6.glyphMap,
   label: string
 };
 
 const navigationRoutes: NavigationOptionType[] = [
-  { href: "/", icon: "dashboard", label: "Dashboard" },
+  { href: "/", icon: "list-alt", label: "Dashboard" },
   { href: "/refuels-list", icon: "filter", label: "Refuels" },
   { href: "/services-list", icon: "wrench", label: "Services" },
   { href: "/settings", icon: "gear", label: "Settings" }
@@ -21,8 +22,8 @@ function NavigationOption({ href, icon, label }: NavigationOptionType) {
   return (
     <Link href={href} asChild style={styles.option}>
       <Pressable>
-        <FontAwesome name={icon} style={styles.icon} />
-        <ThemedText style={styles.label}>{label}</ThemedText>
+        <ThemedIcon lightColor="orangered" name={icon} style={styles.icon} />
+        <ThemedText lightColor="orangered" style={styles.label}>{label}</ThemedText>
       </Pressable>
     </Link>
   );
@@ -43,23 +44,23 @@ export default function Navigation() {
 
 const styles = StyleSheet.create({
   container: {
-    alignSelf: "center",
     flexDirection: "row",
     justifyContent: "center",
-    gap: 10,
+    alignSelf: "center",
+    gap: 8,
+    width: "100%",
     margin: 20
   },
   option: {
     alignItems: "center",
-    width: 70,
-    paddingVertical: 6,
+    width: "18%",
+    paddingVertical: 8,
     borderRadius: 18,
     borderWidth: 1,
     borderColor: "orangered"
   },
   icon: {
-    fontSize: 28,
-    color: "#eee"
+    fontSize: 22
   },
   label: {
     fontSize: 10
