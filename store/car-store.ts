648d687e01606f7ddb1cea624cae2c3ad986ef70
mwawrzen <w.w@op.pl>
2025-10-23 01:08:16 +0200

    feat: update adding car action

--- store/car-store.ts
+++ store/car-store.ts
@@ -1,4 +1,5 @@
-import { FuelEnum, Refuel, Service } from '@/utils/types';
+import { AddCarType, FuelEnum, Refuel, Service } from '@/utils/types';
+import uuid from "react-native-uuid";
 import { create } from 'zustand';
 
 interface Car {
@@ -14,15 +15,27 @@ interface Car {
 interface CarStore {
   cars: Car[];
   currentCar: Car | null;
-  addCar: ( newCar: Car ) => void;
+  addCar: (newCar: AddCarType) => void;
   // editCar: ( id: Car['id'], newCar: Car ) => void;
   // removeCar: ( id: Car['id'] ) => void;
 }
 
-const useCarStore = create<CarStore>(set => ({
+const useCarStore = create<CarStore>()(set => ({
   cars: [],
   currentCar: null,
-  addCar: newCar => set(state => ({ cars: [ ...state.cars, newCar ]})),
+  addCar: newCar => set(state => {
+    const newCarObject: Car = {
+      ...newCar,
+      id: uuid.v4(),
+      refuels: [],
+      services: []
+    };
+    const newState = {
+      cars: [ ...state.cars, newCarObject ],
+      currentCar: state.currentCar || newCarObject
+    };
+    return newState;
+  }),
   // editCar: (id, newCar) => set(state => ({ cars: [ ...state.cars, newCar ]})),
   // removeCar: id => set(state => ({ cars: [ ...state.cars, newCar ]}))
 }));
