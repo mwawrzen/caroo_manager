169867092bb7d7e4c7cea14302c9bb6af6d2d20d
mwawrzen <w.w@op.pl>
2025-10-22 17:31:23 +0200

    feat: add routes

---
+++ app/(tabs)/services-list.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+
+export default function ServicesList() {
+  return (
+    <ThemedView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ThemedText>services list.</ThemedText>
+    </ThemedView>
+  );
+};
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,10 @@
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 
-export default function List() {
+export default function Settings() {
   return (
     <ThemedView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <ThemedText>refuels list.</ThemedText>
+      <ThemedText>settings.</ThemedText>
     </ThemedView>
   );
 };
