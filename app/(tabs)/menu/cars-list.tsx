592bd90fbb5dd2101b6b9510776f7142a9458288
mwawrzen <w.w@op.pl>
2025-10-23 03:02:08 +0200

    feat: create cars list view

---
+++ app/(tabs)/menu/cars-list.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/themed/themed-text";
+import { ThemedView } from "@/components/themed/themed-view";
+
+export default function CarsList() {
+  return (
+    <ThemedView>
+      <ThemedText>cars list.</ThemedText>
+    </ThemedView>
+  );
+};
