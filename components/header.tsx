16b6c5ed3d27a6d08cad4bd709b9eaae2b8fc6d7
mwawrzen <w.w@op.pl>
2025-10-24 01:19:43 +0200

    feat: updating header while changing car name

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,20 @@
 import { ThemedText } from "@/components/themed/themed-text";
 import { ThemedView } from "@/components/themed/themed-view";
+import useCarStore from "@/store/car-store";
 import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import { Pressable, StyleSheet } from "react-native";
 import { ThemedIcon } from "./themed/themed-icon";
 
-export default function Header({ title }: { title?: string }) {
+export default function Header() {
+
+  const { cars, getCurrentCar } = useCarStore();
+  const currentCar = getCurrentCar();
+
+  const [title, setTitle] = useState<string>(currentCar?.name || "Caroo Manager");
 
   const router = useRouter();
-  const color = title ? { color: "orangered" } : {};
-  title = title || "Caroo Manager";
+  const color = currentCar?.name ? { color: "orangered" } : {};
 
   //TODO: fix back button functionality
   function goBack() {
@@ -16,12 +22,18 @@ export default function Header({ title }: { title?: string }) {
       router.back();
   }
 
+  useEffect(() => {
+    const currentCar = getCurrentCar();
+    if (currentCar)
+      setTitle(currentCar.name);
+  }, [cars]);
+
   return (
     <ThemedView style={styles.container}>
       <Pressable onPress={goBack}>
         <ThemedIcon name="arrow-left" style={[color, styles.icon]} />
       </Pressable>
-      <ThemedText type="title" style={[color]}>{title}</ThemedText>
+      <ThemedText type="title" style={color}>{title}</ThemedText>
     </ThemedView>
   );
 };
