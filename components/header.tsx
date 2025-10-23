0aa4f71572794111a91e7e1a5ed728510d5cd7f4
mwawrzen <w.w@op.pl>
2025-10-23 03:01:53 +0200

    feat: add back navigation button

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,27 @@
 import { ThemedText } from "@/components/themed/themed-text";
 import { ThemedView } from "@/components/themed/themed-view";
-import { StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+import { Pressable, StyleSheet } from "react-native";
+import { ThemedIcon } from "./themed/themed-icon";
 
-export default function Header() {
+export default function Header({ title }: { title?: string }) {
+
+  const router = useRouter();
+  const color = title ? { color: "orangered" } : {};
+  title = title || "Caroo Manager";
+
+  //TODO: fix back button functionality
+  function goBack() {
+    if(router.canGoBack())
+      router.back();
+  }
+
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title">Caroo Manager</ThemedText>
+      <Pressable onPress={goBack}>
+        <ThemedIcon name="arrow-left" style={[color, styles.icon]} />
+      </Pressable>
+      <ThemedText type="title" style={[color]}>{title}</ThemedText>
     </ThemedView>
   );
 };
@@ -13,9 +29,13 @@ export default function Header() {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
+    gap: 14,
     width: "100%",
     padding: 28
+  },
+  icon: {
+    marginTop: 6,
+    fontSize: 20
   }
 });
