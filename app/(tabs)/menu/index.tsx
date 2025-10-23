8b0fc8e76e105aa8d37f9a77dd2adc4ee1cf91c1
mwawrzen <w.w@op.pl>
2025-10-23 03:00:36 +0200

    feat: create menu view

---
+++ app/(tabs)/menu/index.tsx
@@ -0,0 +1,74 @@
+import { ThemedIcon } from "@/components/themed/themed-icon";
+import { ThemedText } from "@/components/themed/themed-text";
+import { ThemedView } from "@/components/themed/themed-view";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { Link } from "expo-router";
+import { Pressable, StyleSheet } from "react-native";
+
+type MenuOptionType = {
+  href: any, //! TEMP
+  icon: keyof typeof FontAwesome6.glyphMap,
+  label: string
+};
+
+const menuRoutes: MenuOptionType[] = [
+  { href: "/menu/cars-list", icon: "car", label: "My Cars" },
+  { href: "/menu/cars-list", icon: "car", label: "My Cars2" },
+  { href: "/menu/cars-list", icon: "car", label: "My Cars3" },
+  { href: "/menu/cars-list", icon: "car", label: "My Cars4" }
+];
+
+function MenuOption({ href, icon, label }: MenuOptionType) {
+  return (
+    <Link href={href} asChild>
+      <Pressable>
+        <ThemedView style={styles.menuOptionContainer}>
+          <ThemedIcon name={icon} lightColor="orangered" style={styles.menuOptionIcon} />
+          <ThemedText style={styles.menuOptionLabel}>{label}</ThemedText>
+        </ThemedView>
+      </Pressable>
+    </Link>
+  );
+}
+
+export default function Menu() {
+
+  const menuOptions = menuRoutes.map(({ href, icon, label }: MenuOptionType) => (
+    <MenuOption key={String(label)} href={href} icon={icon} label={label} />
+  ));
+
+  return (
+    <ThemedView style={styles.container}>
+      {/* <ScrollView style={styles.container}> */}
+        {menuOptions}
+      {/* </ScrollView> */}
+    </ThemedView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 10
+  },
+  menuOptionContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+    width: "90%",
+    marginHorizontal: "auto",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 2,
+    borderColor: "orangered",
+    borderRadius: 20
+    // backgroundColor: "darkgreen"
+  },
+  menuOptionIcon: {
+    fontSize: 20
+  },
+  menuOptionLabel: {
+    marginBottom: 4,
+    fontSize: 26
+  },
+});
