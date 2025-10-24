bcf9219795a14afce8aa9d57add96a037623b481
mwawrzen <w.w@op.pl>
2025-10-25 00:51:41 +0200

    feat: redesign preferences view

--- app/(tabs)/menu/preferences.tsx
+++ app/(tabs)/menu/preferences.tsx
@@ -1,62 +1,121 @@
+import Form from "@/components/form";
 import { ThemedText } from "@/components/themed/themed-text";
 import { ThemedView } from "@/components/themed/themed-view";
-import { DistanceUnitEnum, LangEnum } from "@/utils/types";
+import { availableLanguages, availableUnits } from "@/utils/data";
+import { CapacityUnitEnum, DistanceUnitEnum, LangEnum, PriceUnitEnum } from "@/utils/types";
+import React, { useState } from "react";
 import { Pressable, StyleSheet } from "react-native";
-import { De, Gb, Pl } from 'react-native-svg-circle-country-flags';
+import { Languages } from 'react-native-svg-circle-country-flags';
 
 export default function Preferences() {
 
-  function setLanguage(lang: LangEnum) {
-
-  }
-
-  function setDistanceUnit(unit: DistanceUnitEnum) {
-
-  }
-
-  return (
+  (
     <ThemedView style={styles.container}>
       <ThemedText type="subtitle">Language</ThemedText>
       <ThemedView style={styles.sectionContainer}>
         <Pressable style={styles.languageButton} onPress={() => setLanguage( LangEnum.ENGLISH )}>
-          <Gb width={50} height={50} />
+          <Languages.Uk width={50} height={50} />
         </Pressable>
         <Pressable style={styles.languageButton} onPress={() => setLanguage( LangEnum.POLISH )}>
-          <Pl width={50} height={50} />
+          <Languages.Pl width={50} height={50} />
         </Pressable>
         <Pressable style={styles.languageButton} onPress={() => setLanguage( LangEnum.GERMAN )}>
-          <De width={50} height={50} />
+          <Languages.De width={50} height={50} />
         </Pressable>
       </ThemedView>
       <ThemedText type="subtitle">Units</ThemedText>
       <ThemedView style={styles.sectionContainer}>
-        <Pressable style={styles.unitButton} onPress={() => setDistanceUnit( DistanceUnitEnum.KM )}>
+        <Pressable style={styles.unitButton} onPress={()=>{}}>
           <ThemedText style={styles.unitText}>km</ThemedText>
         </Pressable>
-        <Pressable style={styles.unitButton} onPress={() => setDistanceUnit( DistanceUnitEnum.MI )}>
+        <Pressable style={styles.unitButton} onPress={()=>{}}>
           <ThemedText style={styles.unitText}>mi</ThemedText>
         </Pressable>
       </ThemedView>
       <ThemedView style={styles.sectionContainer}>
-        <Pressable style={styles.unitButton} onPress={() => setDistanceUnit( DistanceUnitEnum.KM )}>
+        <Pressable style={styles.unitButton} onPress={()=>{}}>
           <ThemedText style={styles.unitText}>GBP</ThemedText>
         </Pressable>
-        <Pressable style={styles.unitButton} onPress={() => setDistanceUnit( DistanceUnitEnum.MI )}>
+        <Pressable style={styles.unitButton} onPress={()=>{}}>
           <ThemedText style={styles.unitText}>PLN</ThemedText>
         </Pressable>
-        <Pressable style={styles.unitButton} onPress={() => setDistanceUnit( DistanceUnitEnum.MI )}>
+        <Pressable style={styles.unitButton} onPress={()=>{}}>
           <ThemedText style={styles.unitText}>EUR</ThemedText>
         </Pressable>
       </ThemedView>
       <ThemedView style={styles.sectionContainer}>
-        <Pressable style={styles.unitButton} onPress={() => setDistanceUnit( DistanceUnitEnum.KM )}>
+        <Pressable style={styles.unitButton} onPress={()=>{}}>
           <ThemedText style={styles.unitText}>Gallons</ThemedText>
         </Pressable>
-        <Pressable style={styles.unitButton} onPress={() => setDistanceUnit( DistanceUnitEnum.MI )}>
+        <Pressable style={styles.unitButton} onPress={()=>{}}>
           <ThemedText style={styles.unitText}>Liters</ThemedText>
         </Pressable>
       </ThemedView>
     </ThemedView>
+  )
+
+  const [language, setLanguage] = useState<LangEnum>(LangEnum.ENGLISH);
+  const [priceUnit, setPriceUnit] = useState<PriceUnitEnum>(PriceUnitEnum.USD);
+  const [capacityUnit, setCapacityUnit] = useState<CapacityUnitEnum>(CapacityUnitEnum.L);
+  const [distanceUnit, setDistanceUnit] = useState<DistanceUnitEnum>(DistanceUnitEnum.KM);
+
+  const languageItems = availableLanguages.map((lang, i) => (
+    <Form.RadioLang
+      key={i}
+      value={lang}
+      isActive={lang === language}
+      onPress={setLanguage}
+    />
+  ));
+
+  const priceUnitItems = availableUnits.priceUnits.map((unit, i) => (
+    <Form.Radio
+      key={i}
+      label={unit.label}
+      value={unit.value}
+      isActive={unit.value === priceUnit}
+      onPress={setPriceUnit}
+    />
+  ));
+
+  const distanceUnitItems = availableUnits.distanceUnits.map((unit, i) => (
+    <Form.Radio
+      key={i}
+      label={unit.label}
+      value={unit.value}
+      isActive={unit.value === distanceUnit}
+      onPress={setDistanceUnit}
+    />
+  ));
+
+  const capacityUnitItems = availableUnits.capacityUnits.map((unit, i) => (
+    <Form.Radio
+      key={i}
+      label={unit.label}
+      value={unit.value}
+      isActive={unit.value === capacityUnit}
+      onPress={setCapacityUnit}
+    />
+  ));
+
+  return (
+    <Form title="Preferences">
+      <Form.RadioGroup>
+        {languageItems}
+      </Form.RadioGroup>
+      <ThemedText style={styles.subtitle}>Currency unit</ThemedText>
+      <Form.RadioGroup>
+        {priceUnitItems}
+      </Form.RadioGroup>
+      <ThemedText style={styles.subtitle}>Distance unit</ThemedText>
+      <Form.RadioGroup>
+        {distanceUnitItems}
+      </Form.RadioGroup>
+      <ThemedText style={styles.subtitle}>Capacity unit</ThemedText>
+      <Form.RadioGroup>
+        {capacityUnitItems}
+      </Form.RadioGroup>
+    </Form>
   );
 };
 
@@ -85,5 +144,8 @@ const styles = StyleSheet.create({
   },
   unitText: {
     fontSize: 22
+  },
+  subtitle: {
+    textAlign: "center"
   }
 });
