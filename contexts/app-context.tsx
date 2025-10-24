e6155e8673795492d83ff888e6341ce1a216923c
mwawrzen <w.w@op.pl>
2025-10-24 02:44:46 +0200

    feat: remove data context

--- contexts/app-context.tsx
+++
@@ -1,8 +0,0 @@
-import { ActionType, ActionTypeEnum, ContextDataType } from "@/contexts/types";
-
-function appReducer(data: ContextDataType, action: ActionType) {
-  switch (action.type) {
-    case ActionTypeEnum.ADD:
-      return [];
-  }
-}
