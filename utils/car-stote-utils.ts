ef4f19efff129bb99f6f6b5a804a89fe87263749
mwawrzen <w.w@op.pl>
2025-10-25 20:34:25 +0200

    feat: add calculating unit average consumption function

--- utils/car-stote-utils.ts
+++ utils/car-stote-utils.ts
@@ -8,3 +8,8 @@ export function sortRefuelsByDate(refuels: Refuel[]): Refuel[] {
   return refuels.sort((a, b) => b.date.getTime() - a.date.getTime());
 }
 
+export function getAvgConsumption(lastRefuel: Refuel, currentRefuel: AddRefuelType): number | null {
+  if (!lastRefuel || !lastRefuel.fullyRefueled)
+    return null;
+  return (100 * currentRefuel.amountOfFuel) / (currentRefuel.mileage - lastRefuel.mileage);
+}
