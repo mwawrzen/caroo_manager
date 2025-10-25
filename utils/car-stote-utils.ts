d8b8b68c57a0fcdbd75938984aa6cbc42fcdf2cd
mwawrzen <w.w@op.pl>
2025-10-25 20:34:01 +0200

    feat: add refuels sorting function

--- utils/car-stote-utils.ts
+++ utils/car-stote-utils.ts
@@ -4,3 +4,7 @@ export function getSumPrice(refuel: AddRefuelType): number {
   return Number((refuel.unitPrice * refuel.amountOfFuel).toFixed(2));
 };
 
+export function sortRefuelsByDate(refuels: Refuel[]): Refuel[] {
+  return refuels.sort((a, b) => b.date.getTime() - a.date.getTime());
+}
+
