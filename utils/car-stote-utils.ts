28c9ec32d9708e09eaa1ecc8e12cfebfb706b3cb
mwawrzen <w.w@op.pl>
2025-10-25 23:05:47 +0200

    feat: create function calculating summary average for fuel

--- utils/car-stote-utils.ts
+++ utils/car-stote-utils.ts
@@ -8,8 +8,20 @@ export function sortRefuelsByDate(refuels: Refuel[]): Refuel[] {
   return refuels.sort((a, b) => b.date.getTime() - a.date.getTime());
 }
 
-export function getAvgConsumption(lastRefuel: Refuel, currentRefuel: AddRefuelType): number | null {
+export function getUnitAvgConsumption(lastRefuel: Refuel, currentRefuel: AddRefuelType): number | null {
   if (!lastRefuel || !lastRefuel.fullyRefueled)
     return null;
   return (100 * currentRefuel.amountOfFuel) / (currentRefuel.mileage - lastRefuel.mileage);
+}
+
+export function getAvgConsumption(refuels: Refuel[]): number {
+  let length = 0;
+  const sumOfAvgs = refuels.reduce((acc, curr) => {
+    if(curr.avgConsumption) {
+      length++;
+      acc += curr.avgConsumption;
+    }
+    return acc;
+  }, 0);
+  return sumOfAvgs / length;
 }
