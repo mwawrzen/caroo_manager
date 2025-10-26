1948d22c100683423d54475ff835450288a0e978
mwawrzen <w.w@op.pl>
2025-10-26 05:11:05 +0100

    feat: add translation for dashboard

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -7,6 +7,7 @@ import { initReactI18next } from 'react-i18next';
 const resources = {
   en: {
     translation: {
+      //add car
       addCarFormTitle: 'Add a car',
       enterName: 'Enter name',
       enterMileage: 'Enter mileage',
@@ -22,10 +23,30 @@ const resources = {
       mi: 'MILES',
       L: 'LITRES',
       GAL: 'GALLONS',
+
+      //dashboard
+      addServiceLabel: 'Add service',
+      addRefuelLabel: 'Add refuel',
+      generalInfoTitle: 'General info',
+      summaryRefulesTitle: 'Summary for refuels',
+      summaryServicesTitle: 'Summary for services',
+
+      //refuels list
+      refuelsTitle: 'Refuels',
+      addButton: 'Add',
+      fuelItem: 'Fuel',
+      fullyRefueledItem: 'Fully refueled',
+      amountOfFuelItem: 'Amount of fuel',
+      unitPriceItem: 'Unit price',
+      sumPriceItem: 'Sum price',
+      avgConsItem: 'Average consumption',
+      mileageItem: 'Mileage',
+      noteItem: 'Note'
     }
   },
   pl: {
     translation: {
+      //add car
       addCarFormTitle: 'Dodaj samochód',
       enterName: 'Wprowadź nazwę',
       enterMileage: 'Wprowadź przebieg',
@@ -41,6 +62,13 @@ const resources = {
       mi: 'MILE',
       L: 'LITRY',
       GAL: 'GALONY',
+
+      //dashboard
+      addServiceLabel: 'Dodaj serwis',
+      addRefuelLabel: 'Dodaj tankowanie',
+      generalInfoTitle: 'Ogólne informacje',
+      summaryRefulesTitle: 'Podsumowanie tankowań',
+      summaryServicesTitle: 'Podsumowanie serwisów'
     }
   }
 };
@@ -49,6 +77,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
+    fallbackLng: 'en',
     lng: 'pl', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
